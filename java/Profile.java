13b05a23ed98cd44e6ead3429596dec57221c1a0
Afif-Afwan <68459591+Afif-Afwan@users.noreply.github.com>
2020-09-05 13:54:10 +0800

    Update Profile.java

--- java/Profile.java
+++ java/Profile.java
@@ -2,6 +2,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -44,6 +45,9 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mFullName = findViewById(R.id.fullNameTextView);
         mDOB = findViewById(R.id.dateOfBirthTextView);
